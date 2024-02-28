99cae0dcc0573c746c8140679a2f30833cc03ab0
Fadillah Satriya Pamungkas <67313@hq.bni.co.id>
2024-02-28 11:56:16 +0700

    update db

--- initsql/init.sql
+++ initsql/init.sql
@@ -1,15 +1,15 @@
-CREATE SCHEMA IF NOT EXISTS bank_sat;
-create table if not exists bank_sat.data_nasabah
+
+create table if not exists postgres.data_nasabah
 (
     nama_lengkap  varchar,
     alamat        varchar,
     tempat_lahir  varchar,
-    tanggal_lahir date,
+    tanggal_lahir varchar,
     nomor_ktp     varchar,
     nomor_hp      varchar
 );
 
-alter table bank_sat.data_nasabah
+alter table postgres.data_nasabah
     owner to postgres;
 
-INSERT INTO bank_sat.data_nasabah (nama_lengkap, alamat, tempat_lahir, "tanggal_lahir", nomor_ktp, nomor_hp) VALUES ('agung', 'cimahi', 'bandung', '1998-08-29', '12372878', '08931839283');
+INSERT INTO postgres.data_nasabah (nama_lengkap, alamat, tempat_lahir, "tanggal_lahir", nomor_ktp, nomor_hp) VALUES ('agung', 'cimahi', 'bandung', '1998-08-29', '12372878', '08931839283');
